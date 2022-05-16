92ad35ae26d998a804cde9fd8e19e66005a4f14a
Staehler, Michael <michael.staehler@opitz-consulting.com>
2022-05-16 16:29:40 +0200

    changed hostname

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "azurerm_storage_account" "vm-sa" {
 
 resource "azurerm_virtual_machine" "vm-linux" {
   count                            = ! contains(tolist([var.vm_os_simple, var.vm_os_offer]), "WindowsServer") && ! var.is_windows_image ? var.nb_instances : 0
-  name                             = "${var.vm_hostname}-vmLinux-${count.index}"
+  name                             = "${var.vm_hostname}${count.index}"
   resource_group_name              = data.azurerm_resource_group.vm.name
   location                         = coalesce(var.location, data.azurerm_resource_group.vm.location)
   availability_set_id              = azurerm_availability_set.vm.id
